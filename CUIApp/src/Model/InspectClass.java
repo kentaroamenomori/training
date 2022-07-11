7cc2e755abcdc27c03d05c1d5edd362f9ab692b5
kentaroamenomori <kentaro.amenomori@flect.co.jp>
2022-07-12 03:40:48 +0900

    先祖クラスに抽象クラスがあり、さらにその先祖に具象クラスがある時に具象クラスのフィールド・メソッドを取得していなかったバグを修正

--- CUIApp/src/Model/InspectClass.java
+++ CUIApp/src/Model/InspectClass.java
@@ -1,5 +1,8 @@
 package Model;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.Arrays;
 
 public class InspectClass implements ModelInterface {
@@ -13,26 +16,29 @@ public class InspectClass implements ModelInterface {
     @Override
     public String execute(String input) {
         Class<?> clazz;
+        Object instance;
         try {
             clazz = Class.forName(input);
-            clazz.getConstructor(); // デフォルトコンストラクタを持っているか判定
-        } catch (NoSuchMethodException e) {
+            instance = clazz.getConstructor().newInstance();
+        } catch (NoSuchMethodException | InstantiationException e) {
             return "the class doesn't have a default constructor";
         } catch (ClassNotFoundException e) {
             return "class not found";
+        } catch (IllegalAccessException e) {
+            return "failed to access the constructor";
+        } catch (InvocationTargetException e) {
+            return "failed to create an instacne of the specified class";
         }
 
         var fieldResult = "fields:\n";
         var methodResult = "methods:\n";
-        while (true) {
-            // clazzのインスタンスを用意する
-            Object instance;
+        while (clazz != null) {
+            // クラスがデフォルトコンストラクタをもたない場合は処理をスキップ
             try {
-                instance = clazz.getDeclaredConstructor().newInstance();
-            } catch (Exception e) {
-                // デフォルトコンストラクタをもたないクラスの場合はループを抜ける
-                e.printStackTrace();
-                break;
+                clazz.getConstructor();
+            } catch (NoSuchMethodException e) {
+                clazz = clazz.getSuperclass();
+                continue;
             }
 
             var fields = Arrays.asList(clazz.getDeclaredFields());
@@ -40,22 +46,13 @@ public class InspectClass implements ModelInterface {
 
             // fields
             for (var field : fields) {
-                fieldResult += field.toString();
-                try {
-                    field.setAccessible(true);
-    
-                    // fieldの初期値を取得する
-                    var value = " = " + field.get(instance).toString();
-                    fieldResult += value;
-                } catch (IllegalAccessException e) {
-                    e.printStackTrace();
-                }
+                fieldResult += createFieldString(clazz, instance, field);
                 fieldResult += "\n";
             }
 
             // methods
             for (var method : methods) {
-                methodResult += method.toString();
+                methodResult += createMethodString(clazz, method);
                 methodResult += "\n";
             }
 
@@ -64,5 +61,39 @@ public class InspectClass implements ModelInterface {
         }
         
         return fieldResult + "\n" + methodResult;
+    }
+
+    /**
+     * 指定したクラス、フィールドとその初期値をつなげた文字列を返す。
+     * @param clazz
+     * @param instance フィールド初期値の取得にクラスのインスタンスが必要となる
+     * @param field
+     * @return クラス名: フィールド = 初期値
+     */
+    private String createFieldString(Class<?> clazz, Object instance, Field field) {
+        var result = "";
+        result += clazz.getName() + ": " + field.toString();
+
+        try {
+            field.setAccessible(true);
+
+            // fieldの初期値を取得する
+            var value = " = " + field.get(instance).toString();
+            result += value;
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+
+        return result;
+    }
+
+    /**
+     * 指定したクラス名とメソッドを繋げた文字列を返す
+     * @param clazz
+     * @param method
+     * @return クラス名: メソッド
+     */
+    private String createMethodString(Class<?> clazz, Method method) {
+        return clazz.getName() + ": " + method.toString();
     }
 }
