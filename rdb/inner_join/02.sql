c3c73547016dd7db34cd4045752290726093856c
kentaroamenomori <kentaro.amenomori@flect.co.jp>
2022-07-12 17:14:22 +0900

    直積とINNER JOIN 問題２INNER JOINその１

---
+++ rdb/inner_join/02.sql
@@ -0,0 +1,13 @@
+-- 従業員テーブルに部署IDをカラムを追加
+ALTER TABLE employees ADD COLUMN division_id text;
+
+-- 部署IDが01の部署の従業員を取得（INNER JOINなし）
+SELECT divisions.id, employees.name 
+	FROM divisions, employees 
+	WHERE divisions.id = employees.division_id AND divisions.id = '01';
+
+-- 部署IDが01の部署の従業員を取得（INNER JOINあり）
+SELECT divisions.name, employees.name 
+	FROM divisions 
+		INNER JOIN employees ON divisions.id = employees.division_id
+		WHERE divisions.id = '01';
