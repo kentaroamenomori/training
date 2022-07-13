606b4228bf73e62bb1c7bc9c58c5d62383e2fd59
kentaroamenomori <kentaro.amenomori@flect.co.jp>
2022-07-13 17:14:17 +0900

    typeを大文字に変更・外部キー制約を追加

--- rdb/outer_join/00_create_table.sql
+++ rdb/outer_join/00_create_table.sql
@@ -1,5 +1,14 @@
 -- 事務所テーブルを作成
-CREATE TABLE offices(id integer NOT NULL, name text NOT NULL, address text);
+CREATE TABLE offices(
+	id INTEGER NOT NULL, 
+	name TEXT NOT NULL, 
+	address TEXT
+);
 
 -- 従業員テーブルに勤務先事務所IDカラムを追加
-ALTER TABLE employees ADD COLUMN office_id integer;
+ALTER TABLE employees ADD COLUMN office_id INTEGER;
+
+-- 外部キー制約を追加
+ALTER TABLE employees
+	ADD CONSTRAINT office_id
+	FOREIGN KEY(office_id) REFERENCES offices(id);
