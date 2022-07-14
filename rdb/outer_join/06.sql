0b34ba6a49551438391281477731bb55a22371e9
kentaroamenomori <kentaro.amenomori@flect.co.jp>
2022-07-14 16:50:02 +0900

    テーブルに別名を付与

--- rdb/outer_join/06.sql
+++ rdb/outer_join/06.sql
@@ -1,7 +1,10 @@
 -- 一つの部署を指定して部署と従業員と従業員が持つ資格を一覧表示する（資格を保有していない従業員も表示する）
-SELECT divisions.name, employees.name, certificates.name 
-	FROM divisions 
-		INNER JOIN employees ON divisions.id = employees.division_id 
-		LEFT OUTER JOIN employee_certificates ON employees.id = employee_certificates.employee_id 
-		LEFT OUTER JOIN certificates ON employee_certificates.certificate_id = certificates.id 
-	WHERE divisions.id = '03';
+SELECT d.name, e.name, c.name 
+	FROM divisions d
+		INNER JOIN employees e
+			ON d.id = e.division_id 
+		LEFT OUTER JOIN employee_certificates ec
+			ON e.id = ec.employee_id 
+		LEFT OUTER JOIN certifications c
+			ON ec.certificate_id = c.id 
+	WHERE d.id = '03';
