25b39ae88c80c684a1d2df3db578fda7e88bb03f
kentaroamenomori <kentaro.amenomori@flect.co.jp>
2022-07-14 15:58:56 +0900

    テーブルに別名を付与

--- rdb/outer_join/05.sql
+++ rdb/outer_join/05.sql
@@ -1,4 +1,4 @@
 -- 全ての従業員とその勤務先事務所を取得する（勤務先事務所がない従業員も表示する）
-SELECT employees.name, offices.name
-	FROM employees
-		LEFT OUTER JOIN offices ON employees.office_id = offices.id;
+SELECT e.name, o.name
+	FROM employees e
+		LEFT OUTER JOIN offices o ON e.office_id = o.id;
