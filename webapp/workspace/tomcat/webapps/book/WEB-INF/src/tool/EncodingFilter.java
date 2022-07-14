a6d9a1a7d14a678ff5b2c95d3aebc5ccf1765e14
kentaroamenomori <kentaro.amenomori@flect.co.jp>
2022-07-15 02:30:15 +0900

    第十章・.gitignoreの修正

---
+++ webapp/workspace/tomcat/webapps/book/WEB-INF/src/tool/EncodingFilter.java
@@ -0,0 +1,25 @@
+package tool;
+
+import java.io.IOException;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebFilter;
+
+@WebFilter(urlPatterns = {"/*"})
+public class EncodingFilter implements Filter {
+
+    @Override
+    public void doFilter(
+        ServletRequest request, ServletResponse response, FilterChain chain
+    ) throws IOException, ServletException {
+        request.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html; charset=UTF-8");
+
+        chain.doFilter(request, response);
+    }
+    
+}
